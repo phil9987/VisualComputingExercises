c7024b746d42fed735b3e84ae40cde4b7b90a966
Klein  Kevin <kklein@ethz.ch>
2016-09-29 11:41:54 +0200

    1b

--- Exercise1/extract_foreground.m
+++ Exercise1/extract_foreground.m
@@ -1,29 +1,42 @@
 function [  ] = extract_foreground(  )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+    % constant representing the number of user inputs
+    iterations = 10;
     v = VideoReader('bluescreen.avi');
     frame1 = readFrame(v);  % read first frame
     whos frame1             % print available infos about frame
     imshow(frame1);         % show first frame for pixel-selection
-    [x,y] = ginput(1);      % let user select a background-pixel
+    % x,y: iterations * 2 integer 
+    [x,y] = ginput(iterations);      % let user select a background-pixel
     x = cast(x, 'uint64');  % cast from double to int
     y = cast(y, 'uint64'); 
-    color = frame1(y,x,:);  % get color of selected pixel
-    color = squeeze(color) % remove singleton dimensions
+    % The following returns 300x1
+    % color(:) = frame1(y,x,:);  % get color of selected pixel
+    % The following returns 10x10x3
+    % colorTest = frame1(y,x,:);
+    % makeshift workaround
+    % color is 10x3
+    color(1:iterations,1:3) = 0;
+    for i=1:iterations
+       color(i,:) = frame1(y(i),x(i),:); 
+    end
+    color = squeeze(color); % remove singleton dimensions
     color = im2double(color);
-    frame1d = im2double(frame1);
+    % this is basically a cast to doubles, permitting later subtraction
+    frame1Double = im2double(frame1);
+  
+    covariance = cov(color);
+    meanValue = mean(color);
+    background = frame1Double;
+    background(:,:,1) = meanValue(1);
+    background(:,:,2) = meanValue(2);
+    background(:,:,3) = meanValue(3);
     
-    background = ones(size(frame1), 'double');
-    background(:,:,1) = color(1);
-    background(:,:,2) = color(2);
-    background(:,:,3) = color(3);
-    
-    D = frame1d - background;
-    E = sqrt(sum(D.^2,3));
-    
-    size(E)
-    %E = D(:,:,1) + D(:,:,2) + D(:,:,3);
-    mask = E < 0.5;         % calculate mask from selected pixel
+    D = (frame1Double - background)' * pinv(covariance) * (frame1Double - background);
+    %E = sqrt(sum(D.^2,3));
+  
+    mask = D < 0.5;         % calculate mask from selected pixel
     
     surrounding = imread('mask.bmp');
     mask2 = surrounding(:,:,1) < 255;      % get surrounding mask
@@ -33,8 +46,5 @@ function [  ] = extract_foreground(  )
     mask3d(:,:,3) = mask;
     frame1(find(mask3d>=1)) = 0;           % set background pixels to (0,0,0) (black)
     imshow(frame1)
-    
-    
-    
 end
 
