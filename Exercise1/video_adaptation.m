1f9fa2a4ba1ab43f3a9e8953b8ac658a4ca13bea
kkleindev <kevin.klein.94@hotmail.de>
2016-10-03 17:07:17 +0200

    restructuring of 2nd task

--- Exercise1/video_adaptation.m
+++ Exercise1/video_adaptation.m
@@ -20,6 +20,7 @@ covariances = ones(n,m,k,k);
 observations = ones(k,l);
 for i=1:n
     for j=1:m
+        % 10/03 Kevin: this returns a dimension mismatch I do not understand
         observations(:,:) = frames(i,j,:,:);
         % Transpose in order to match the observation patter (row = event)
         observations = observations';
@@ -32,27 +33,38 @@ end
 
 % adapt all actual frames
 % retrieve and save all frames
-
 i = 1;
 while hasFrame(videoTest)
     frames(:,:,:,i) = readFrame(videoTest);
     i = i+1;
 end
+
 % iterate over all frames
 [n,m,k,l] = size(frames);
-outputFrame = ones(n,m,k,l);
+% outputFrame = ones(n,m,k,l);
 mahaDistances = ones(n,m);
 for frameCount = 1:l
     %iterate over all pixels per frame
     for i=1:n
         for j=1:m
             % compute maha distance for this pixel
+            currentMean = means(i,j,:);
+            invertedCovariance = pinv(covariances(i,j,:,:));
+            aux = reshape(frames(i,j,:,l), [3,1]);
+            diff = aux - currentMean;
+            mahaDistances(i,j) = diff' * invertedCovariance * diff;
         end
     end
 
     % mask this image based on mahaDistances matrix
-    outputFrame(:,:,:,frameCount) = 0;
+    mask = mahaDistanceMatrix > 100;         %the distances differ extremely heavily per execution, therefore use of a dependent term is advisable
+    mask3d = mask;                      % turn mask to 3 dimensions to use find below
+    mask3d(:,:,2) = mask;
+    mask3d(:,:,3) = mask;
+    nextFrame = im2uint8(frames(:,:,:,frameCount).*mask3d);
+    writeVideo(vw,nextFrame);
 end
+
 
 
 end
